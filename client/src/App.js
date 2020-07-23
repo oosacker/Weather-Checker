21957f55e8a5444986ece4be6a174b222fc74875
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-23 19:19:38 +1200

    add server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-class App extends Component {
-state = {
-    data: null
-  };
+function App () {
+  const [myData, setData] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+  async function getData() {
+    console.log('getData');
+
+    await fetch(`/get_data`)
+      .then(res => res.json())
+      .then(
+        result => {
+          setData(result);
+          console.log(result);
+          setIsLoaded(true);
+        },
+        error => {
+          alert(error)
+        }
+      )
+      .catch (error => {
+        alert(error);
+      })
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
 
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
+  useEffect(() => {
+    getData();
+  },[]);
 
-  render() {
+
+  if (!isLoaded) {
+    console.log('Waiting');
+    return (
+      <div>Waiting...</div>
+    )
+  } 
+  else {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data 
-        <p className="App-intro">{this.state.data}</p>
+        {myData.data}
       </div>
     );
   }
-}
+};
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
+app.get('/get_data', (req, res) => {
   res.send({ 
-    express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' 
+    data: 'This is your data!!' 
   });
 });
