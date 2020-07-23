0abd624cd40d424c07beb9d32bfcf4829a4c8218
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-24 11:35:47 +1200

    corrected the style

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React, {useState, useEffect} from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+import './styles/output.css';
 
 function App () {
-  const [myData, setData] = useState(null);
+  const [weatherData, setWeather] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [city, setCity] = useState('city');
+  const [isError, setError] = useState(false);
 
   async function getData() {
     console.log('getData');
@@ -11,7 +15,7 @@ function App () {
       .then(res => res.json())
       .then(
         result => {
-          setData(result);
+          setWeather(result);
           console.log(result);
           setIsLoaded(true);
         },
@@ -28,6 +32,14 @@ function App () {
     getData();
   },[]);
 
+  const handleChange = (event) => {
+    setCity(event.target.value.toLowerCase());
+  }
+
+  const handleSubmit = (event) => {
+    getData();
+    event.preventDefault();
+  }
 
   if (!isLoaded) {
     console.log('Waiting');
@@ -37,25 +49,30 @@ function App () {
   } 
   else {
     return (
-      <div className="App">
-        
-        {myData && (
-          <div align={'center'} justify={'center'} className={'row-2'}>
-              <div className={'weather-result'}>
-                <h3>{myData.name.toUpperCase()}</h3>
-                <p>Temperature: {myData.main.temp} degC</p>
-                <p>Feels like: {myData.main.feels_like} degC</p>
-                <p>Humidity: {myData.main.humidity} %</p>
+      <Container className={'main-container'}>
+        <Row align={'center'} justify={'center'} className={'row-1'}>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='city_input'>Enter city: </label>
+            <input id='city_input' type='input'  name='city' onChange={handleChange} size="50"/>
+            <input type="submit" value="Submit" />
+          </form>
+        </Row>
+
+        {weatherData && (
+          <Row align={'center'} justify={'center'} className={'row-2'}>
+              <Container className={'weather-result'}>
+                <h3>{weatherData.name.toUpperCase()}</h3>
+                <p>Temperature: {weatherData.main.temp} degC</p>
+                <p>Feels like: {weatherData.main.feels_like} degC</p>
+                <p>Humidity: {weatherData.main.humidity} %</p>
                 <div className='weather-icon-container'>
-                  <img className={'weather-icon'} src={`http://openweathermap.org/img/wn/${myData.weather[0].icon}.png`}></img>
+                  <img className={'weather-icon'} src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img>
                 </div>
-                <p>{myData.weather[0].main +': '+ myData.weather[0].description} </p>
-              </div>
-          </div>
+                <p>{weatherData.weather[0].main +': '+ weatherData.weather[0].description} </p>
+              </Container>
+          </Row>
         )}
-
-
-      </div>
+      </Container>
     );
   }
 };
