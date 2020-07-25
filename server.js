1977c2764070a4402934815caa1305356e1cf565
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-25 15:26:06 +1200

    add concurrently script

--- server.js
+++ server.js
@@ -1,27 +1,58 @@
 const express = require('express');
 const fetch = require('node-fetch');
-var bodyParser = require('body-parser');
-
+const moment = require('moment');
 const app = express();
 
 require('dotenv').config();
 const API_KEY = process.env.WEATHER_API_KEY;
-const port = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+
+// console.log that your server is up and running
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+
+//middleware
+const logger = (req, res, next) => {
+  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} @ ${moment().format()}`);
+  next();
+}
+app.use(logger);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 let weatherData = null;
 let city = 'wellington';
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// test code
+const testarray = [
+  {
+    id: 0,
+    name: "natsuki",
+    age: 36
+  },
+  {
+    id: 1,
+    name: "bob",
+    age: 55
+  },
+  {
+    id: 2,
+    name: "andy",
+    age: 90
+  },
+];
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.get('/test', (req, res) => {
+  res.json(testarray);
+})
 
 
-app.post('/post-test', (req, res) => {
+app.post('/post_test', (req, res) => {
   console.log('Got body:', req.body);
   console.log(req.body['username']);
-  res.sendStatus(200);
+  // res.sendStatus(200);
+  res.send(req.body);
 });
 
 // create a GET route -- MUST BE MARKED async and await so you can until valid data is available!!
