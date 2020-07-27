034f5a15596ed0f27e581b30a5d0c6deca8736f0
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-27 18:59:40 +1200

    add comments

--- client/src/App.js
+++ client/src/App.js
@@ -95,6 +95,13 @@ const App = () => {
 
   return (
     <Container className={'main-container'}>
+
+      <Container className={'title-box'}>
+        <h1 className={'title'}>Weather Checker v2</h1>
+        <h3 className={'sub-title'}>Natsuki Hasegawa, 2020</h3>
+        <p>A simple web app that demonstrates a Node.js/Express.js backend server and a React frontend. The backend fetches the weather data for the selected city from OpenWeatherMap's API. The backend itself also implements a RESTful API for the frontend interface. The data are transferred in JSON format.</p>
+      </Container>
+    
       <Container className={'selection-box'}>
       {countryList && (
         <Row align={'center'} justify={'center'} className={'row-1'}>
@@ -125,12 +132,15 @@ const App = () => {
         <Container className={'weather-result'}>
           <Row align={'center'} justify={'center'} className={'row-3'}>
             <Container className={'weather-result-inner'}>
-              <h3>{weatherData.name.toUpperCase()}</h3>
+              <h3 className={'city-name'}>{weatherData.name.toUpperCase()}</h3>
               <p>Temperature: {weatherData.main.temp} degC</p>
               <p>Feels like: {weatherData.main.feels_like} degC</p>
               <p>Humidity: {weatherData.main.humidity} %</p>
               <div className='weather-icon-container'>
-                <img className={'weather-icon'} src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}></img>
+                <img 
+                  className={'weather-icon'} 
+                  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}>
+                </img>
               </div>
               <p>{weatherData.weather[0].main +': '+ weatherData.weather[0].description} </p>
             </Container>
