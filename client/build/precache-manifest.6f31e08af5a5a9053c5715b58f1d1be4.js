783b9169a87fe619ddb1905e0f86fc7f94a5591e
Natsuki Hasegawa <oosacker@gmail.com>
2020-07-28 11:01:25 +1200

    deploy

---
+++ client/build/precache-manifest.6f31e08af5a5a9053c5715b58f1d1be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e7926b9fcf125af9e538faa7b03cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f43c1d71359681541431",
+    "url": "/static/css/main.aa9924a9.chunk.css"
+  },
+  {
+    "revision": "8488c5f4eb2cb2973c33",
+    "url": "/static/js/2.3e1a1a21.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.3e1a1a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f43c1d71359681541431",
+    "url": "/static/js/main.eb2dedc9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
